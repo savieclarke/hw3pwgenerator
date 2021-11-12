f24be5f652eb17bb441b7d3fcc91dfcb214b2e0a
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2021-11-12 03:14:32 -0500

    got the length prompt to come back up

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("you clicked the button")
 
+  var length = parseInt(window.prompt("How long do you want your password? Characters 8-128"));
+
 
   return "the generated password"
-}
+
+};
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
