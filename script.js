ee3f0d9af0def5b2a08abcc1e46c1eb6842c2ff6
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2021-11-16 11:15:51 -0500

    trying concat

--- script.js
+++ script.js
@@ -4,29 +4,57 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("you clicked the button")
 
+  var numberchar = "1234567890";
+  var upperchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerchar = "abcdefghijklmnopqrustvwxyz";
+  var specialchar = "~!@#$%^&*()_+[]|/?.<>,:";
+
   var length = parseInt(window.prompt("How long do you want your password? Characters 8-128"));
   if (!length) {
-    alert("error", "Needs a length")
-
-
+    alert("error - Needs a length")
+return;
   }
+
 
   var lowercase = confirm("Do you want lowercase?");
 
   if (!lowercase) {
-    alert("error", "Needs a length")
-
+    alert("error - wee need to know if you want lowercase")
+return;
 
   }
 
 
   var uppercase = confirm("Do you want uppercase?");
 
+  if (!uppercase) {
+    alert("error - we need to know if you want uppercase")
+    return;
+
+  }
+
   var numbers = confirm("Do you want numbers?");
+
+  if (!numbers) {
+    alert("error - we need to know if you want numbers")
+    return;
+
+
+  }
 
   var characters = confirm("Do you want special characters?");
 
-  
+  if (!characters) {
+    alert("error - we need to know if you want numbers")
+    return;
+
+
+  }
+
+  if (promptLength, confirmUppercase, confirmNumbers, confirmCharacters) {
+
+    choices = character.concat (lowerchar,upperchar,numberchar,specialchar);
+  }
 
   return "the generated password"
 
