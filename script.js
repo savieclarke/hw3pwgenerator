c6b1ed184714be6e33fedc4fd92fcd54822b100d
Savannah Clarke <92831573+savieclarke@users.noreply.github.com>
2021-11-30 21:35:16 -0500

    realized I already have the length specifications from the windows prompt

--- script.js
+++ script.js
@@ -4,61 +4,68 @@ var generateBtn = document.querySelector("#generate");
 function generatePassword() {
   console.log("you clicked the button")
 
-  var numberchar = "1234567890";
-  var upperchar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var lowerchar = "abcdefghijklmnopqrustvwxyz";
-  var specialchar = "~!@#$%^&*()_+[]|/?.<>,:";
+  // var numberchar = "1234567890";
+  var numberchar = ["1", "2", "4", "5", "6", "7", "8", "9", "0"];
+  var upperchar = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowerchar = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "u", "s", "t", "v", "w", "x", "y", "z"];
+  var specialchar = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "[", "]", "|", "?", ".", "<", ">", ","];
+  var choices = [];
+  var actualchoices = [];
 
   var length = parseInt(window.prompt("How long do you want your password? Characters 8-128"));
   if (!length) {
     alert("error - Needs a length")
-return;
+    return;
   }
 
-
-  var lowercase = confirm("Do you want lowercase?");
-
-  if (!lowercase) {
-    alert("error - wee need to know if you want lowercase")
-return;
+  if (length > 128) {
+    alert("error - Must be less than 128 characters")
+    return;
 
   }
 
+  var lowercase = confirm("Do you want lowercase?"); // true or false
+  // falsey - false, 0, "", null, undefined
+  if (lowercase) {
+    choices = choices.concat(lowerchar)
+  }
 
   var uppercase = confirm("Do you want uppercase?");
 
-  if (!uppercase) {
-    alert("error - we need to know if you want uppercase")
-    return;
-
+  if (uppercase) {
+    choices = choices.concat(upperchar)
   }
 
   var numbers = confirm("Do you want numbers?");
 
-  if (!numbers) {
-    alert("error - we need to know if you want numbers")
+  if (numbers) {
+    choices = choices.concat(numberchar)
+  }
+
+  var specials = confirm("Do you want special characters?");
+
+  if (specials) {
+    choices = choices.concat(specialchar)
+  }
+
+  if (!lowercase && !uppercase && !numbers && !specials) {
+    alert("You must choose at least one category of characters!");
     return;
+  }
+  // generate random password using choices array
+  var password = "";
 
-
+  for (var i = 0; i < length; i++) {
+  
+    password += choices[Math.floor(Math.random() * choices.length)]
   }
 
-  var characters = confirm("Do you want special characters?");
-
-  if (!characters) {
-    alert("error - we need to know if you want numbers")
-    return;
-
-
-  }
-
-  if (promptLength, confirmUppercase, confirmNumbers, confirmCharacters) {
-
-    choices = character.concat (lowerchar,upperchar,numberchar,specialchar);
-  }
-
-  return "the generated password"
+    
+  return password
 
 };
+  
+
 
 
 
@@ -69,7 +76,7 @@ return;
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
+  console.log(passwordText);
   passwordText.value = password;
 
 }
